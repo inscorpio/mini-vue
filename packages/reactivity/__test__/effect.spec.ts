336a92f3e0bcfacbded0405ab726c988d089e72e
Linyx <inscorpio@icloud.com>
2023-07-30 20:42:27 +0800

    chore: scheduler

--- packages/reactivity/__test__/effect.spec.ts
+++ packages/reactivity/__test__/effect.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { reactive } from '../src/reactive'
 import { effect } from '../src/effect'
 
@@ -26,5 +26,27 @@ describe('effect', () => {
 
     counter.num = 1
     expect(runner()).toBe(1)
+  })
+
+  // Question:
+  // 1. 在 Vue 源码中 哪里使用到了 scheduler?
+
+  // 1. 如果传入 scheduler , 初始化调用 fn 不调用 scheduler
+  // 2. 更新时不调用 effectFn 而是调用 scheduler
+  it('scheduler', () => {
+    let dummy
+    const observed = reactive({ foo: 0 })
+    const scheduler = vi.fn()
+    effect(
+      () => dummy = observed.foo,
+      { scheduler },
+    )
+
+    expect(dummy).toBe(0)
+    expect(scheduler).not.toHaveBeenCalled()
+
+    observed.foo = 1
+    expect(dummy).toBe(0)
+    expect(scheduler).toBeCalledTimes(1)
   })
 })
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,14 +1,37 @@
 type EffectFn = () => void
+type EffectScheduler = () => void
+interface EffectOptions {
+  scheduler?: EffectScheduler
+}
+// 这个类型也不知道取什么名字，根据源码看，这里其实需要封装一个 ReactiveEffect 的类
+// Question:
+// 1. 为什么取名为 ReactiveEffect
+// 2. 为什么要封装成 class? 是源码中有多次使用吗？
+interface ReactiveEffect {
+  fn: EffectFn
+  scheduler?: EffectScheduler
+}
 
-let activeFn: EffectFn
+let activeEffect: ReactiveEffect
 
-export function effect(fn: EffectFn) {
-  activeFn = fn
-  fn()
+export function effect(fn: EffectFn, options?: EffectOptions) {
+  const { scheduler } = options ?? {}
+  // 暂时不知道给这个对象取什么名字, 但是根据源码来看，它还是一个 _effect
+
+  // Question:
+  // 1. 为什么这里已经有一个 effect 函数了，还要给这个对象取名为 _effect
+
+  activeEffect = {
+    fn,
+    scheduler,
+  }
+
+  activeEffect.fn()
+
   return fn
 }
 
-const targetMap = new Map<object, Map<unknown, Set<EffectFn>>>()
+const targetMap = new Map<object, Map<unknown, Set<ReactiveEffect>>>()
 
 // 收集函数(fn)
 // 一个 key 有多个 fn 且不会重复，所以使用 Set 的数据结构
@@ -20,22 +43,28 @@ export function track(target: object, key: unknown) {
     targetMap.set(target, keyMap)
   }
 
-  let fnSet = keyMap.get(key)
+  let effectSet = keyMap.get(key)
 
-  if (!fnSet) {
-    fnSet = new Set()
-    keyMap.set(key, fnSet)
+  if (!effectSet) {
+    effectSet = new Set()
+    keyMap.set(key, effectSet)
   }
 
-  fnSet.add(activeFn)
+  // 最开始只是为了收集 fn, 如果这里添加的是 activeEffect, 那么叫 fnSet 就不合适了
+  effectSet.add(activeEffect)
 }
 
 // 触发函数(fn)
 export function trigger(target: object, key: unknown) {
-  // targetMap -> keyMap -> fnSet
+  // targetMap -> keyMap -> effectSet
 
   const keyMap = targetMap.get(target)
-  const fnSet = keyMap.get(key)
+  const effectSet = keyMap.get(key)
 
-  fnSet.forEach(fn => fn())
+  effectSet.forEach((effect) => {
+    // 一开始只是为了执行 effectFn, 但是现在还需要执行 scheduler, 那收集依赖的时候除了收集 effectFn 之外，就还需要收集对应的 scheduler, 所以 在 track 中应该做出改变
+    effect.scheduler
+      ? effect.scheduler()
+      : effect.fn()
+  })
 }
