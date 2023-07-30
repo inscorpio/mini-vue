3af5b45ca5477295aa200855ca7b4ea8725a192e
Linyx <inscorpio@icloud.com>
2023-07-30 16:17:40 +0800

    chore: add question

--- packages/reactivity/__test__/effect.spec.ts
+++ packages/reactivity/__test__/effect.spec.ts
@@ -15,6 +15,8 @@ describe('effect', () => {
     expect(dummy).toBe(9)
   })
 
+  // Question: 为什么需要返回这个 runner 呢？
+  // 目前并没有体现出它的作用
   it('should return a runner when call effect', () => {
     const counter = reactive({ num: 0 })
 
