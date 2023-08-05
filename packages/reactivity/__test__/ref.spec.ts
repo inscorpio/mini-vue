9fa2a4e47c42ecadb030a0f51118caedaa3e897a
Linyx <inscorpio@icloud.com>
2023-08-05 12:42:51 +0800

    feat: add isRef & unref

--- packages/reactivity/__test__/ref.spec.ts
+++ packages/reactivity/__test__/ref.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it, vi } from 'vitest'
 import { effect } from '../src/effect'
-import { ref } from '../src/ref'
+import { isRef, ref, unref } from '../src/ref'
 
 describe('ref', () => {
   // 将传入的值通过 .value 的形式访问
@@ -50,5 +50,19 @@ describe('ref', () => {
     expect(dummy).toBe(undefined)
     a.value = 2
     expect(dummy).toBe(2)
+  })
+
+  it('isRef', () => {
+    expect(isRef(ref(1))).toBe(true)
+
+    expect(isRef(0)).toBe(false)
+    expect(isRef(1)).toBe(false)
+    // an object that looks like a ref isn't necessarily a ref
+    expect(isRef({ value: 0 })).toBe(false)
+  })
+
+  it('unref', () => {
+    expect(unref(1)).toBe(1)
+    expect(unref(ref(1))).toBe(1)
   })
 })
--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -3,12 +3,20 @@ import type { ReactiveEffect } from './effect'
 import { isTracking, trackEffects, triggerEffects } from './effect'
 import { reactive } from './reactive'
 
+interface Ref<T = any> {
+  value: T
+}
+
+type MaybeRef<T> = T | Ref<T>
+
 class RefImpl<T> {
   private _value: T
 
   // 重点理解 ref 只有一个 value key 的 dep, 不用像 reactive 那样根据不同的 key 存 deps
   // 依赖收集和触发依赖逻辑与 reactive 一致
   public dep = new Set<ReactiveEffect>()
+  public __v_isRef = true
+
   constructor(value) {
     this._value = isObject(value)
       ? reactive(value) // 如果初始值是对象，应该将对象转化为响应式的
@@ -30,4 +38,16 @@ class RefImpl<T> {
 
 export function ref<T>(value?: T) {
   return new RefImpl<T>(value)
+}
+
+// 不是很理解这里的类型处理和函数重载
+export function isRef<T>(ref: Ref<T> | unknown): ref is Ref<T>
+export function isRef(ref: any): ref is Ref {
+  return !!ref.__v_isRef
+}
+
+export function unref<T>(ref: MaybeRef<T>): T {
+  return isRef(ref)
+    ? ref.value
+    : ref
 }
