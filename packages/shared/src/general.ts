d05393b841d8a7ad00e1bf43c7fc0e98feac61a1
Linyx <inscorpio@icloud.com>
2023-08-13 13:13:12 +0800

    fix<shared>: isObject

--- packages/shared/src/general.ts
+++ packages/shared/src/general.ts
@@ -1,5 +1,5 @@
 export const getType = value => Object.prototype.toString.call(value).slice(8, -1)
 
-export const isObject = (value: unknown) => getType(value) === 'Object'
+export const isObject = (value: unknown) => value !== null && typeof value === 'object'
 
 export const extend = Object.assign
